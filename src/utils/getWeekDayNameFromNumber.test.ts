0be665da00d71256c05140d656fce451af3f91c2
Belyaev <sergei.beleaev@gmail.com>
2022-09-14 11:48:55 +0300

    test: Add test for module getWeekDayNameNumber

---
+++ src/utils/getWeekDayNameFromNumber.test.ts
@@ -0,0 +1,27 @@
+import { WeekDay } from '../timer/Timer';
+import { getWeekDayNameFromNumber } from './getWeekDayNameFromNumber';
+
+describe('getWeekDayNameFromNumber module:', () => {
+
+  describe.each([
+    [0, WeekDay.Sunday],
+    [1, WeekDay.Monday],
+    [2, WeekDay.Tuesday],
+    [3, WeekDay.Wednesday],
+    [4, WeekDay.Thursday],
+    [5, WeekDay.Friday],
+    [6, WeekDay.Saturday],
+  ])('getWeekDayNameFromNumber(%i)', (dayNumber, dayName) => {
+    it(`return ${dayName}`, () => {
+      expect(getWeekDayNameFromNumber(dayNumber)).toBe(dayName);
+    });
+  });
+
+  it('day > 6 should be throw Error', () => {
+    expect(() => getWeekDayNameFromNumber(7)).toThrowError();
+  });
+
+  it('day < 0 should be throw Error', () => {
+    expect(() => getWeekDayNameFromNumber(-2)).toThrowError();
+  });
+});
